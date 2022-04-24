a2f07ef4f499d993090019d8855a679164fb9e3d
dwild <dwild@ucsc.edu>
2022-04-24 12:29:25 -0700

    add body attempt 1

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -16,6 +16,8 @@ class Play extends Phaser.Scene {
         this.background = this.add.tileSprite(0, 0, 256, 256, 'bg').setOrigin(0,0).setScale(3);
         this.WoodL = this.add.tileSprite(-160, 0, 260, 1300, 'ab').setOrigin(0,0);
         this.WoodR = this.add.tileSprite(game.canvas.width-100, 0, 260, 1300, 'ab').setOrigin(0,0);
+        this.matter.add.gameObject(this.WoodL);
+        this.matter.add.gameObject(this.WoodR);
         this.add.text(game.canvas.width/2, game.canvas.height/3, "Play State").setOrigin(0.5,0);
         this.player = this.add.ellipse(game.canvas.width/2, game.canvas.height/2, 75, 75, 0x6666ff);
         this.speed = 0;
@@ -23,7 +25,7 @@ class Play extends Phaser.Scene {
     }
 
  update(){
-
+    
     this.speed += .08;
     this.WoodL.tilePositionY += this.speed;
     this.WoodR.tilePositionY += this.speed;
