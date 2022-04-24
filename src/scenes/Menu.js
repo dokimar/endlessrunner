c7fba278656884114c1418986f051a866060b3fc
SkylerHaataja <sdhaataj@ucsc.edu>
2022-04-24 13:00:51 -0700

    fixed Menu loop

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -17,9 +17,14 @@ class Menu extends Phaser.Scene {
       this.button.setInteractive();
       const title = this.add.text(game.canvas.width/2, game.canvas.height/4, "Slug Fall").setOrigin(0.5,0);
       title.setFontSize(100);
+      this.start = false;
       
    }
    update(){
+      if(this.start == true){
+         this.scene.start('playScene');
+         this.start = false;
+      }
       this.background.tilePositionY += .2;
       this.button.on("pointerover", () => {
          this.button = this.add.image(game.canvas.width/2, game.canvas.height/2, 'button', 2);
@@ -31,7 +36,7 @@ class Menu extends Phaser.Scene {
 
       this.button.on("pointerdown", () => {
          this.button = this.add.image(game.canvas.width/2, game.canvas.height/2, 'button', 0);
-         this.scene.start('playScene');
+         this.start = true;
       });
       
       
