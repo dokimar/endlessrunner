50a44279b33cfba0a4525ba740c8e49df5fbd22b
SkylerHaataja <sdhaataj@ucsc.edu>
2022-04-24 17:55:55 -0700

    Some temp platforms

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -9,7 +9,9 @@ class Play extends Phaser.Scene {
     preload(){
         
         this.load.image('ab', 'assets/WoodSides.png');
+        this.load.image('platA', 'assets/platformA.png');
         this.load.image('bg', 'assets/bg.png');
+        this.load.image('slug', 'assets/Slug.png');
     }
     
     create(){
@@ -20,25 +22,36 @@ class Play extends Phaser.Scene {
 
         this.WoodR = this.add.tileSprite(game.canvas.width, 0, 260, 1439, 'ab').setOrigin(0,0);
         
-        this.matter.add.gameObject(this.WoodL).setFrictionAir(0.001);
-        this.matter.add.gameObject(this.WoodR).setFrictionAir(0.001);
+        this.matter.add.gameObject(this.WoodL).setFrictionAir(0.001).setStatic(true).setFriction(0);;
+        this.matter.add.gameObject(this.WoodR).setFrictionAir(0.001).setStatic(true);
         
         this.speed = 0;
 
         this.add.text(game.canvas.width/2, game.canvas.height/3, "Play State").setOrigin(0.5,0);
-        this.player = this.add.ellipse(game.canvas.width/2, game.canvas.height/2, 75, 75, 0x6666ff);
-        this.matter.add.gameObject(this.player).setFrictionAir(0.001);
+        this.player = this.add.sprite(game.canvas.width/2, game.canvas.height/2, 'slug');
+        this.matter.add.gameObject(this.player).setFrictionAir(0.001).setFriction(1000);
         console.log("hello");
+
+        this.P1 = new Platform(this,0,120, 'platA', 1);
+        this.matter.add.gameObject(this.P1).setFrictionAir(0.001).setIgnoreGravity(true).setStatic(true).setFriction(100);
+        this.P1.setAngle(25);
+        this.P1.y = 1000;
     }
 
     update(){
-        if(this.speed < 20){
-            this.speed += .001
+        this.P1.update();
+        if(this.player.body.velocity.y >= 20){
+            this.player.body.velocity.y = 20;
         }
+        this.player.setAngle(0);
 
-        this.background.tilePositionY += this.speed;
-        this.WoodL.tilePositionY += this.speed;
-        this.WoodR.tilePositionY += this.speed;
+        this.background.tilePositionY += this.player.body.velocity.y/4;
+        this.WoodL.tilePositionY += this.player.body.velocity.y;
+        this.WoodR.tilePositionY += this.player.body.velocity.y;
+        if(this.player.y >= 400){
+            this.player.y = 399;
+        }
+        
 
  }
  
