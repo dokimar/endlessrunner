9f698f614c0a48901b9c0be9be291ce6ab643b6a
Marlene Lopez <mlope109@ucsc.edu>
2022-05-01 03:37:47 -0700

    adding clear instructions on menu screen

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -11,6 +11,22 @@ class Menu extends Phaser.Scene {
    }
    
    create(){
+      let textConfig = {
+         fontFamily: 'Courier',
+         fontSize: '28px',
+         //backgroundColor: '#F3B141',
+         color: '#FF7F50',
+         align: 'center',
+         padding: {
+             top: 5,
+             bototm: 5,
+         },
+         stroke: '#000000',
+         strokeThickness: 5,
+         fixedWidth: 0
+      }
+
+
       this.background = this.add.tileSprite(0, 0, 300, 256, 'bg').setOrigin(0,0).setScale(3);
       
       this.button = this.add.image(game.canvas.width/2, game.canvas.height/2, 'button', 1);
@@ -18,9 +34,28 @@ class Menu extends Phaser.Scene {
       const title = this.add.text(game.canvas.width/2, game.canvas.height/4, "Slug Fall").setOrigin(0.5,0);
       title.setFontSize(100);
       this.start = false;
+
+
+      let centerX = game.config.width/2;
+      let centerY = game.config.height/2;
+      let textSpacer = 48;
+
+
+      // game instructions text
+      this.add.text(centerX, centerY + 2 * textSpacer, 'Move with the Arrow Keys (←)(→)', textConfig).setOrigin(0.5);
+      this.add.text(centerX, centerY + 3 * textSpacer, 'Press (Q) to Break Through Webs', textConfig).setOrigin(0.5);
+      // Goal text
+      textConfig.color = '#FFFF66';
+      this.add.text(centerX, centerY + 4 * textSpacer, '>> Avoid the Bird and Falling too Fast <<', textConfig).setOrigin(0.5);
+      // credits line text
+      textConfig.color = '#5F9EA0';
+      textConfig.fontSize= '14px';
+      this.add.text(centerX, centerY + 7 * textSpacer, 'Game by Skyler Haataja, Marlene Lopez, and Daniel Wild', textConfig).setOrigin(0.5);
+
       
    }
    update(){
+
       if(this.start == true){
          this.scene.start('playScene');
          this.start = false;
@@ -38,8 +73,6 @@ class Menu extends Phaser.Scene {
          this.button = this.add.image(game.canvas.width/2, game.canvas.height/2, 'button', 0);
          this.start = true;
       });
-      
+      }
       
    }
-
-}
